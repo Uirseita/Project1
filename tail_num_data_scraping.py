3fa1d2665848924e904c151c80f44ae58b3de7a8
Uirseita <qingyu_ye@brown.edu>
2018-10-19 01:51:02 -0400

    minor changes

--- tail_num_data_scraping.py
+++ tail_num_data_scraping.py
@@ -1,19 +1,20 @@
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import re
 
 
-def find_match(tail_num, df):
-    for i in range(df.shape[0]):
-        if df.loc[i, 'old_tail_num'] == tail_num:
-            return df.loc[i, 'new_tail_num']
+def find_match(t_num, conv_df):
+    for i in range(conv_df.shape[0]):
+        if conv_df.loc[i, 'old_tail_num'] == t_num:
+            return conv_df.loc[i, 'new_tail_num']
+        else:
+            pass
     return None
 
 
-def scrape(tail_num):
-    assert isinstance(tail_num, str)
+def scrape(t_num):
+    assert isinstance(t_num, str)
     id_list = {'mfr_name': 'content_lbMfrName',
                'model': 'content_Label7',
                'year': 'content_Label17',
@@ -34,11 +35,11 @@ def scrape(tail_num):
 
     column_list = ['tail_num']
     column_list.extend(key_list)
-    df = pd.DataFrame(columns=column_list)
-    df.loc[0, 'tail_num'] = tail_num
+    scrape_df = pd.DataFrame(columns=column_list)
+    scrape_df.loc[0, 'tail_num'] = t_num
     response = requests.get(
         'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx'
-        '?NNumbertxt=' + tail_num
+        '?NNumbertxt=' + t_num
     )
     soup = BeautifulSoup(response.content, "html.parser")
     not_retired = False
@@ -52,7 +53,7 @@ def scrape(tail_num):
         for key in key_list:
             s = soup.find('span', id=id_list[key]).string.strip()
             if s != 'None':
-                df.loc[0, key] = s
+                scrape_df.loc[0, key] = s
             else:
                 pass
     else:
@@ -66,28 +67,31 @@ def scrape(tail_num):
             for key in ret_key_list:
                 s = soup.find('span', id=ret_id_list[key]).string.strip()
                 if s != 'None':
-                    df.loc[0, key] = s
+                    scrape_df.loc[0, key] = s
                 else:
                     pass
         else:
             pass
-    if isinstance(df.loc[0, 'cert_year'], str):
-        df.loc[0, 'cert_year'] = re.search(r'\w+/\w+/(\w+)',
-                                           df.loc[0, 'cert_year']
-                                           ).group(1).strip()
-    return df
-
-
-tail_num_df = pd.read_csv('./tail_num/tail_num_list.csv')
-tail_num_convert = pd.read_csv('./tail_num/tail_num_convert.csv')
-tail_num_list = tail_num_df['tail_num'].tolist()
-df = pd.DataFrame()
-for tail_num in tail_num_list:
-    # tail num has to be converted
-    if isinstance(find_match(tail_num, tail_num_convert), str):
-        result = scrape(find_match(tail_num, tail_num_convert))
-        result.loc[0, 'tail_num'] = tail_num
-        df = pd.concat([df, result], ignore_index=True)
+    if isinstance(scrape_df.loc[0, 'cert_year'], str):
+        scrape_df.loc[0, 'cert_year'] = re.search(r'\w+/\w+/(\w+)',
+                                                  scrape_df.loc[0, 'cert_year']
+                                                  ).group(1).strip()
     else:
-        df = pd.concat([df, scrape(tail_num)], ignore_index=True)
-df.to_csv('./tail_num/new_all_tail_num.csv', index=False)
+        pass
+    return scrape_df
+
+
+if __name__ == "__main__":
+    tail_num_df = pd.read_csv('./tail_num/tail_num_list.csv')
+    tail_num_convert = pd.read_csv('./tail_num/tail_num_convert.csv')
+    tail_num_list = tail_num_df['tail_num'].tolist()
+    df = pd.DataFrame()
+    for tail_num in tail_num_list:
+        # tail num has to be converted
+        if isinstance(find_match(tail_num, tail_num_convert), str):
+            result = scrape(find_match(tail_num, tail_num_convert))
+            result.loc[0, 'tail_num'] = tail_num
+            df = pd.concat([df, result], ignore_index=True)
+        else:
+            df = pd.concat([df, scrape(tail_num)], ignore_index=True)
+    df.to_csv('./tail_num/new_all_tail_num.csv', index=False)
