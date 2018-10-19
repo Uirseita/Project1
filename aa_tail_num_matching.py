bfd5f9fde97c85a45fc3143419c88d6f069c1858
Uirseita <qingyu_ye@brown.edu>
2018-10-18 23:52:08 -0400

    put all NxxxAA tail numbers into convert list

--- aa_tail_num_matching.py
+++ aa_tail_num_matching.py
@@ -1,38 +1,48 @@
 import numpy as np
 import pandas as pd
 
-# for incorrect or mismatched tail numbers which miss all information
-tail_num_df = pd.read_csv('./tail_num/tail_num.csv')
-df_missing = tail_num_df[tail_num_df.iloc[:, 1:5].isnull().all(axis=1)]
-df_conv_tail_num = pd.DataFrame()
-df_conv_tail_num['old_tail_num'] = df_missing['tail_num']
-df_conv_tail_num['new_tail_num'] = np.nan
-aa_df = pd.read_csv('./tail_num/aa_current_fleet_tail_num_convert.csv')
+
+df = pd.read_csv('flight_data.csv')
+tail_num = pd.Series(df['TAIL_NUM'].unique()).dropna()
+tail_num_df = pd.DataFrame()
+# create a new DataFrame for tail num
+tail_num_df['tail_num'] = tail_num
+# extract all NxxxAA tail numbers
+df_aa = tail_num_df[tail_num_df['tail_num'].str.match(r'^N\w{3}AA$')]
+df_tail_num_convert = pd.read_csv('./tail_num/tail_num_convert.csv')
+df_aa = df_aa.rename(index=str, columns={"tail_num": "old_tail_num"})
+df_aa['new_tail_num'] = np.nan
+df_aa = df_aa.reset_index(drop=True)
+aa_current = pd.read_csv('./tail_num/aa_current_fleet_tail_num_convert.csv')
 # match AA current fleet
-for i in range(df_conv_tail_num.shape[0]):
-    for j in range(aa_df.shape[0]):
-        if df_conv_tail_num.loc[i, 'old_tail_num'] \
-                == aa_df.loc[j, 'old_tail_num']:
-            df_conv_tail_num.loc[i, 'new_tail_num'] \
-                = aa_df.loc[j, 'new_tail_num']
+for i in range(df_aa.shape[0]):
+    for j in range(aa_current.shape[0]):
+        if df_aa.loc[i, 'old_tail_num'] \
+                == aa_current.loc[j, 'old_tail_num']:
+            df_aa.loc[i, 'new_tail_num'] \
+                = aa_current.loc[j, 'new_tail_num']
             break
-aa_ret_df = pd.read_csv('./tail_num/aa_retired_fleet_tail_num_convert.csv')
+aa_ret = pd.read_csv('./tail_num/aa_retired_fleet_tail_num_convert.csv')
 # match AA retire fleet
-for i in range(df_conv_tail_num.shape[0]):
-    if not isinstance(df_conv_tail_num.loc[i, 'new_tail_num'], str):
-        for j in range(aa_ret_df.shape[0]):
-            if df_conv_tail_num.loc[i, 'old_tail_num'] \
-                    == aa_ret_df.loc[j, 'old_tail_num']:
-                df_conv_tail_num.loc[i, 'new_tail_num'] \
-                    = aa_ret_df.loc[j, 'new_tail_num']
+for i in range(df_aa.shape[0]):
+    if not isinstance(df_aa.loc[i, 'new_tail_num'], str):
+        for j in range(aa_ret.shape[0]):
+            if df_aa.loc[i, 'old_tail_num'] \
+                    == aa_ret.loc[j, 'old_tail_num']:
+                df_aa.loc[i, 'new_tail_num'] \
+                    = aa_ret.loc[j, 'new_tail_num']
                 break
 # match AA current fleet ignore US/AA in the end
-for i in range(df_conv_tail_num.shape[0]):
-    if not isinstance(df_conv_tail_num.loc[i, 'new_tail_num'], str):
-        for j in range(aa_df.shape[0]):
-            if df_conv_tail_num.loc[i, 'old_tail_num'][:-2] \
-                    == aa_df.loc[j, 'old_tail_num'][:-2]:
-                df_conv_tail_num.loc[i, 'new_tail_num'] \
-                    = aa_df.loc[j, 'new_tail_num']
+for i in range(df_aa.shape[0]):
+    if not isinstance(df_aa.loc[i, 'new_tail_num'], str):
+        for j in range(aa_current.shape[0]):
+            if df_aa.loc[i, 'old_tail_num'][:-2] \
+                    == aa_current.loc[j, 'old_tail_num'][:-2]:
+                df_aa.loc[i, 'new_tail_num'] \
+                    = aa_current.loc[j, 'new_tail_num']
                 break
-df_conv_tail_num.to_csv('./tail_num/tail_num_convert.csv', index=False)
+df_aa = df_aa.dropna(how='any')
+df_tail_num_convert = pd.concat([df_tail_num_convert, df_aa])
+df_tail_num_convert = df_tail_num_convert.drop_duplicates().reset_index(
+    drop=True)
+df_tail_num_convert.to_csv('./tail_num/tail_num_convert.csv', index=False)
