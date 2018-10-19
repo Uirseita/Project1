baf14653e6e06ca043e098d5fb1dba1a7b5c8b5e
Uirseita <qingyu_ye@brown.edu>
2018-10-19 01:42:41 -0400

    rewrite the code, add cert_year column

--- tail_num_data_scraping.py
+++ tail_num_data_scraping.py
@@ -4,312 +4,90 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-df = pd.read_csv('flight_data.csv')
-tail_num = pd.Series(df['TAIL_NUM'].unique()).dropna()
-tail_num_df = pd.DataFrame()
-# create a new DataFrame for tail num
-tail_num_df['tail_num'] = tail_num
-tail_num = list(tail_num)
-mfr_name = []
-model = []
-year = []
-eng_mfr_name = []
-eng_model = []
-aircraft_type = []
-for url in tail_num:
-    if isinstance(url, str):
-        response = requests.get(
-            'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx'
-            '?NNumbertxt=' + url
-        )
-        soup = BeautifulSoup(response.content, "html.parser")
-        # determine if the planes has retired
-        if soup.find('span', id='content_lbMfrName') or \
-                soup.find('span', id='content_Label7') or \
-                soup.find('span', id='content_Label17'):
-            # determine if there is a MFR name
-            if soup.find('span', id='content_lbMfrName').string != 'None':
-                mfr_name.append(soup.find(
-                    'span', id='content_lbMfrName'
-                ).string.strip())
-            else:
-                mfr_name.append(np.nan)
-            # determine if there is a model
-            if soup.find('span', id='content_Label7').string != 'None':
-                model.append(soup.find(
-                    'span', id='content_Label7'
-                ).string.strip())
-            else:
-                model.append(np.nan)
-            # determine if there is a MFR year
-            if soup.find('span', id='content_Label17').string != 'None':
-                year.append(int(soup.find(
-                    'span', id='content_Label17'
-                ).string.strip()))
-            # use the year in Certificate Issue Date
-            elif soup.find('span', id='content_lbCertDate').string != 'None':
-                year.append(int(re.search(
-                    r'\w+/\w+/(\w+)', soup.find(
-                        'span', id='content_lbCertDate'
-                    ).string
-                ).group(1).strip()))
-            else:
-                 year.append(np.nan)
-            # determine if there is an eng mfr name
-            if soup.find('span', id='content_lbEngMfr').string != 'None':
-                eng_mfr_name.append(soup.find(
-                    'span', id='content_lbEngMfr'
-                ).string.strip())
-            else:
-                eng_mfr_name.append(np.nan)
-            # determine if there is an eng model
-            if soup.find('span', id='content_lbEngModel').string != 'None':
-                eng_model.append(soup.find(
-                    'span', id='content_lbEngModel'
-                ).string.strip())
-            else:
-                eng_model.append(np.nan)
-            # determine if there is a type
-            if soup.find('span', id='content_Label11').string != 'None':
-                aircraft_type.append(
-                    soup.find('span', id='content_Label11').string.strip())
-            else:
-                aircraft_type.append(np.nan)
-        # the plane has retired
-        elif soup.find(
-                'span', id='content_drptrDeRegAircraft_lbDeRegMfrName_0'
-        ) or soup.find(
-            'span', id='content_drptrDeRegAircraft_lbDeRegModel_0'
-        ) or soup.find(
-            'span', id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'
-        ):
-            if soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegMfrName_0'
-            ).string != 'None':
-                mfr_name.append(soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegMfrName_0'
-                ).string.strip())
-            else:
-                mfr_name.append(np.nan)
-            # determine if there is a model
-            if soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegModel_0'
-            ).string != 'None':
-                model.append(soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegModel_0'
-                ).string.strip())
-            else:
-                model.append(np.nan)
-            # determine if there is a MFR year
-            if soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'
-            ).string != 'None':
-                year.append(int(soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'
-                ).string.strip()))
-            # use the year in Certificate Issue Date
-            elif soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDeRegCertDate_0'
-            ).string != 'None':
-                year.append(int(re.search(
-                    r'\w+/\w+/(\w+)', soup.find(
-                        'span',
-                        id='content_drptrDeRegAircraft_lbDeRegCertDate_0'
-                    ).string
-                ).group(1).strip()))
-            else:
-                 year.append(np.nan)
-            # determine if there is an eng mfr name
-            if soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDREngMfr_0'
-            ).string != 'None':
-                eng_mfr_name.append(soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDREngMfr_0'
-                ).string.strip())
-            else:
-                eng_mfr_name.append(np.nan)
-            # determine if there is an eng model
-            if soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDREngModel_0'
-            ).string != 'None':
-                eng_model.append(soup.find(
-                    'span', id='content_drptrDeRegAircraft_lbDREngModel_0'
-                ).string.strip())
-            else:
-                eng_model.append(np.nan)
-            aircraft_type.append(np.nan)
-        else:
-            mfr_name.append(np.nan)
-            model.append(np.nan)
-            year.append(np.nan)
-            eng_mfr_name.append(np.nan)
-            eng_model.append(np.nan)
-            aircraft_type.append(np.nan)
-    else:
-        mfr_name.append(np.nan)
-        model.append(np.nan)
-        year.append(np.nan)
-        eng_mfr_name.append(np.nan)
-        eng_model.append(np.nan)
-        aircraft_type.append(np.nan)
-tail_num_df['mfr_name'] = mfr_name
-tail_num_df['model'] = model
-tail_num_df['year'] = year
-tail_num_df['eng_mfr_name'] = eng_mfr_name
-tail_num_df['eng_model'] = eng_model
-tail_num_df['aircraft_type'] = aircraft_type
-tail_num_df.to_csv('./tail_num/tail_num.csv', index=False)
-# match tail nums that missing all information
-tail_num = pd.read_csv('./tail_num/tail_num_convert.csv')
-tail_num_lst = tail_num['new_tail_num'].tolist()
-tail_num_df = pd.DataFrame()
-tail_num_df['tail_num'] = tail_num['old_tail_num']
-for url in tail_num_lst:
-    if isinstance(url, str):
-        response = requests.get(
-            'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx?'
-            'NNumbertxt=' + url
-        )
-        soup = BeautifulSoup(response.content, "html.parser")
-        # determine if the planes has retired
-        if soup.find('span', id='content_lbMfrName') \
-                or soup.find('span', id='content_Label7') or soup.find(
-                'span', id='content_Label17'):
-            # determine if there is a MFR name
-            if soup.find('span', id='content_lbMfrName').string != 'None':
-                mfr_name.append(
-                    soup.find('span', id='content_lbMfrName').string.strip())
-            else:
-                mfr_name.append(np.nan)
-            # determine if there is a model
-            if soup.find('span', id='content_Label7').string != 'None':
-                model.append(
-                    soup.find('span', id='content_Label7').string.strip())
-            else:
-                model.append(np.nan)
-            # determine if there is a MFR year
-            if soup.find('span', id='content_Label17').string != 'None':
-                year.append(int(
-                    soup.find('span', id='content_Label17').string.strip()))
-            # use the year in Certificate Issue Date
-            elif soup.find('span', id='content_lbCertDate').string != 'None':
-                year.append(int(re.search(
-                    r'\w+/\w+/(\w+)',
-                    soup.find('span', id='content_lbCertDate').string
-                ).group(1).strip()))
-            else:
-                year.append(np.nan)
-            # determine if there is an eng mfr name
-            if soup.find('span', id='content_lbEngMfr').string != 'None':
-                eng_mfr_name.append(
-                    soup.find('span', id='content_lbEngMfr').string.strip())
-            else:
-                eng_mfr_name.append(np.nan)
-            # determine if there is an eng model
-            if soup.find('span', id='content_lbEngModel').string != 'None':
-                eng_model.append(
-                    soup.find('span', id='content_lbEngModel').string.strip())
-            else:
-                eng_model.append(np.nan)
 
-            # determine if there is a type
-            if soup.find('span', id='content_Label11').string != 'None':
-                aircraft_type.append(
-                    soup.find('span', id='content_Label11').string.strip())
-            else:
-                aircraft_type.append(np.nan)
+def find_match(tail_num, df):
+    for i in range(df.shape[0]):
+        if df.loc[i, 'old_tail_num'] == tail_num:
+            return df.loc[i, 'new_tail_num']
+    return None
 
-        # the plane has retired
-        elif soup.find('span',
-                       id='content_drptrDeRegAircraft_lbDeRegMfrName_0') \
-                or soup.find('span',
-                             id='content_drptrDeRegAircraft_lbDeRegModel_0') \
-                or soup.find(
-                'span', id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'):
-            if soup.find('span',
-                         id='content_drptrDeRegAircraft_lbDeRegMfrName_0'
-                         ).string != 'None':
-                mfr_name.append(
-                    soup.find('span',
-                              id='content_drptrDeRegAircraft_lbDeRegMfrName_0'
-                              ).string.strip())
-            else:
-                mfr_name.append(np.nan)
-            # determine if there is a model
-            if soup.find('span',
-                         id='content_drptrDeRegAircraft_lbDeRegModel_0'
-                         ).string != 'None':
-                model.append(
-                    soup.find('span',
-                              id='content_drptrDeRegAircraft_lbDeRegModel_0'
-                              ).string.strip())
-            else:
-                model.append(np.nan)
-            # determine if there is a MFR year
-            if soup.find('span',
-                         id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'
-                         ).string != 'None':
-                year.append(int(
-                    soup.find('span',
-                              id='content_drptrDeRegAircraft_lbDeRegYearMfr_0'
-                              ).string.strip()))
-            # use the year in Certificate Issue Date
-            elif soup.find('span',
-                           id='content_drptrDeRegAircraft_lbDeRegCertDate_0'
-                           ).string != 'None':
-                year.append(int(re.search(
-                    r'\w+/\w+/(\w+)', soup.find(
-                        'span',
-                        id='content_drptrDeRegAircraft_lbDeRegCertDate_0'
-                    ).string
-                ).group(1).strip()))
-            else:
-                year.append(np.nan)
-            # determine if there is an eng mfr name
-            if soup.find('span',
-                         id='content_drptrDeRegAircraft_lbDREngMfr_0').string != 'None':
-                eng_mfr_name.append(
-                    soup.find('span',
-                              id='content_drptrDeRegAircraft_lbDREngMfr_0'
-                              ).string.strip())
-            else:
-                eng_mfr_name.append(np.nan)
-            # determine if there is an eng model
-            if soup.find('span',
-                         id='content_drptrDeRegAircraft_lbDREngModel_0'
-                         ).string != 'None':
-                eng_model.append(
-                    soup.find('span',
-                              id='content_drptrDeRegAircraft_lbDREngModel_0'
-                              ).string.strip())
-            else:
-                eng_model.append(np.nan)
-            aircraft_type.append(np.nan)
+
+def scrape(tail_num):
+    assert isinstance(tail_num, str)
+    id_list = {'mfr_name': 'content_lbMfrName',
+               'model': 'content_Label7',
+               'year': 'content_Label17',
+               'cert_year': 'content_lbCertDate',
+               'eng_mfr_name': 'content_lbEngMfr',
+               'eng_model': 'content_lbEngModel',
+               'aircraft_type': 'content_Label11'
+               }
+    key_list = list(id_list.keys())
+    ret_id_list = {'mfr_name': 'content_drptrDeRegAircraft_lbDeRegMfrName_0',
+                   'model': 'content_drptrDeRegAircraft_lbDeRegModel_0',
+                   'year': 'content_drptrDeRegAircraft_lbDeRegYearMfr_0',
+                   'cert_year': 'content_drptrDeRegAircraft_lbDeRegCertDate_0',
+                   'eng_mfr_name': 'content_drptrDeRegAircraft_lbDREngMfr_0',
+                   'eng_model': 'content_drptrDeRegAircraft_lbDREngModel_0',
+                   }
+    ret_key_list = list(ret_id_list.keys())
+
+    column_list = ['tail_num']
+    column_list.extend(key_list)
+    df = pd.DataFrame(columns=column_list)
+    df.loc[0, 'tail_num'] = tail_num
+    response = requests.get(
+        'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx'
+        '?NNumbertxt=' + tail_num
+    )
+    soup = BeautifulSoup(response.content, "html.parser")
+    not_retired = False
+    for i in range(3):
+        if soup.find('span', id=id_list[key_list[i]]):
+            not_retired = True
         else:
-            mfr_name.append(np.nan)
-            model.append(np.nan)
-            year.append(np.nan)
-            eng_mfr_name.append(np.nan)
-            eng_model.append(np.nan)
-            aircraft_type.append(np.nan)
+            pass
+
+    if not_retired:
+        for key in key_list:
+            s = soup.find('span', id=id_list[key]).string.strip()
+            if s != 'None':
+                df.loc[0, key] = s
+            else:
+                pass
     else:
-        mfr_name.append(np.nan)
-        model.append(np.nan)
-        year.append(np.nan)
-        eng_mfr_name.append(np.nan)
-        eng_model.append(np.nan)
-        aircraft_type.append(np.nan)
-tail_num_df['mfr_name'] = mfr_name
-tail_num_df['model'] = model
-tail_num_df['year'] = year
-tail_num_df['eng_mfr_name'] = eng_mfr_name
-tail_num_df['eng_model'] = eng_model
-tail_num_df['aircraft_type'] = aircraft_type
-tail_num_df.to_csv('./tail_num/matched_tail_num.csv', index=False)
-tail_num_df_1 = pd.read_csv('./tail_num/tail_num.csv')
-# drop all the NaN rows in first df
-idx = np.array(tail_num_df_1.iloc[:, 1:5].dropna(how='all').index).tolist()
-tail_num_df_1 = tail_num_df_1.loc[idx]
-tail_num_df_1 = pd.concat([tail_num_df_1, tail_num_df])
-tail_num_df_1.reset_index(drop=True)
-tail_num_df_1.to_csv('./tail_num/all_tail_num.csv', index=False)
+        is_dereg = False
+        for i in range(3):
+            if soup.find('span', id=ret_id_list[ret_key_list[i]]):
+                is_dereg = True
+            else:
+                pass
+        if is_dereg:
+            for key in ret_key_list:
+                s = soup.find('span', id=ret_id_list[key]).string.strip()
+                if s != 'None':
+                    df.loc[0, key] = s
+                else:
+                    pass
+        else:
+            pass
+    if isinstance(df.loc[0, 'cert_year'], str):
+        df.loc[0, 'cert_year'] = re.search(r'\w+/\w+/(\w+)',
+                                           df.loc[0, 'cert_year']
+                                           ).group(1).strip()
+    return df
+
+
+tail_num_df = pd.read_csv('./tail_num/tail_num_list.csv')
+tail_num_convert = pd.read_csv('./tail_num/tail_num_convert.csv')
+tail_num_list = tail_num_df['tail_num'].tolist()
+df = pd.DataFrame()
+for tail_num in tail_num_list:
+    # tail num has to be converted
+    if isinstance(find_match(tail_num, tail_num_convert), str):
+        result = scrape(find_match(tail_num, tail_num_convert))
+        result.loc[0, 'tail_num'] = tail_num
+        df = pd.concat([df, result], ignore_index=True)
+    else:
+        df = pd.concat([df, scrape(tail_num)], ignore_index=True)
+df.to_csv('./tail_num/new_all_tail_num.csv', index=False)
